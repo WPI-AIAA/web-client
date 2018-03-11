6752fea109b18e0007ffbd55e2ce2e6b190b1e9a
Robaire <robaire@galliath.com>
2018-03-11 16:04:17 -0400

    Moved fetch into child

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,35 +1,18 @@
 import React, { Component } from 'react';
 
-import Lounge from './Lounge'
+import Location from './Location'
 
 class Login extends Component {
 
     constructor() {
         super();
-
-        var studentList = [];
-    }
-
-    componentWillMount(){
-
-
-        fetch('http://localhost:8080/stats/students')
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (studentList) {
-                //console.log(studentList);
-                for(let student of studentList){
-                    studentList.push(student);
-                }
-            });
-
     }
 
     render() {
         return(
+
             <div className="Login">
-                <Lounge students={this.studentList}/>
+                    <Location locationName={"lounge"} sourceURL={this.props.sourceURL}/>
             </div>
         );
     }
