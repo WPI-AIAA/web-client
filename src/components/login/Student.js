e2d5940feb5121b99a9d98655fb3dac0f1ec7e95
Robaire <robaire@galliath.com>
2018-03-11 16:03:02 -0400

    Added Kick functionality

--- src/components/login/Student.js
+++ src/components/login/Student.js
@@ -4,17 +4,24 @@ class Student extends Component {
 
     constructor() {
         super();
+
+        this.kick = this.kick.bind(this);
+    }
+
+    kick() {
+        // Pass this up to the parent
+        this.props.onStudentKick(this.props.student);
     }
 
     render() {
-
         return(
             <li className = "Student">
                 <span>
                     {this.props.student.firstName + ' '}
-                    
                     {this.props.student.lastName}
                 </span>
+
+                <button onClick={this.kick}> Kick </button>
             </li>
         );
     }
