c993c9687ca0959a64c64b67ed378bae806439e5
Robaire <robaire@galliath.com>
2018-03-12 10:47:54 -0400

    Adding routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 import Navbar from './components/navbar/Navbar';
+import Home from './components/home/Home';
 import Login from './components/login/Login';
+import About from './components/about/About';
+import Schedule from './components/schedule/Schedule';
+import PageNotFound from './components/pagenotfound/PageNotFound';
 
 class App extends Component {
 
@@ -20,7 +25,15 @@ class App extends Component {
     return (
       <div className="App">
         <Navbar />
-        <Login sourceURL={this.state.sourceURL}/>
+
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/about" component={About}/>
+          <Route exact path="/login" render={props => <Login sourceURL={this.state.sourceURL}/>} />
+          <Route exact path="/schedule" component={Schedule} />
+          <Route component={PageNotFound} />
+        </Switch>
       </div>
     );
 
