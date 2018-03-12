5f25cb7d9d270546fc5d39bafa6b70cd693dd437
Robaire <robaire@galliath.com>
2018-03-12 10:50:18 -0400

    Created empty pages to route to

---
+++ src/components/pagenotfound/PageNotFound.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import './PageNotFound.css';
+
+class PageNotFound extends Component {
+
+    render() {
+
+        return (
+            <div className="PageNotFound">
+                <h1> Page Not Found </h1>
+            </div>
+        );
+
+    }
+}
+
+export default PageNotFound;
---
+++ src/components/schedule/Schedule.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Schedule extends Component {
+
+    render() {
+
+        return (
+            <div className="Schedule">
+                <h1> Schedule </h1>
+            </div>
+        );
+
+    }
+}
+
+export default Schedule;
