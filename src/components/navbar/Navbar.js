12ea557f5c599d599848ad235b241ad810727575
Robaire <robaire@galliath.com>
2018-03-12 10:48:34 -0400

    Added additional links

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+
 import './Navbar.css';
 
 class Navbar extends Component {
-
-    constructor() {
-        super();
-    }
 
     render() {
 
         return (
             <div className="Navbar">
-                <h1> NAVBAR </h1>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+
+                    <span className="navbar-brand">Navbar</span>
+
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item active">
+                                <Link className="nav-link" to='/home'> Home </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to='/about'> About </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to='/schedule'> Schedule </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to='/login'> Login </Link>
+                            </li>
+                        </ul>
+                    </div>
+
+                </nav>
             </div>
-        );
-
-    }
-}
-
-export default Navbar;
+                );
+        
+            }
+        }
+        
+        export default Navbar;
