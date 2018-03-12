44563f315193ccf84948d735f24cedc418632e4b
Robaire <robaire@galliath.com>
2018-03-12 10:48:15 -0400

    Added <BrowserRouter/>

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 // Style Sheets
 import './bootstrap.css';
@@ -7,4 +8,8 @@ import './bootstrap.css';
 // Components
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+, document.getElementById('root'));
