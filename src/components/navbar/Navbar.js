6ba1a4de8bca61b930027ab97554f48da7ac7926
Robaire Galliath <robaire@galliath.com>
2018-04-26 22:47:12 -0400

    Updated Navbar styling

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -12,21 +12,38 @@ class Navbar extends Component {
             <div className="Navbar">
 
                 <div className="container">
-                    <div className="row">
-                        <img className="banner-img" src="/images/banners/banner-white-accent.svg" alt="logo" style={{width: '70%'}}/>
-                    </div>
-                    <div className="row">
-                        <nav className="nav">
-                            <span className="nav-link"> <Link className="link" to='/home'> <p className="nav-text">     Home    </p> </Link> </span>
-                            <span className="nav-link"> <Link className="link" to='/about'> <p className="nav-text">    About   </p> </Link> </span>
-                            <span className="nav-link"> <Link className="link" to='/events'> <p className="nav-text">   Events  </p> </Link> </span>
-                            <span className="nav-link"> <Link className="link" to='/login'> <p className="nav-text">    Login   </p> </Link> </span>
-                        </nav>
-                    </div>
 
+                    <nav className="navbar navbar-expand-lg navbar-light">
+                        <img className="banner-img navbar-brand" src="/images/branding/banners/banner-white-accent.svg" alt="logo" style={{width: '20vw'}}/>
 
+                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"/>
+                        </button>
+
+                        <div className="collapse navbar-collapse" id="navbarNav">
+                            <ul className="navbar-nav">
+                                <li className="nav-item active">
+                                    <Link className="nav-link" to='/home'>      <p className="nav-text">    HOME    </p> </Link>
+                                    <span className="pipe"/>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to='/about'>     <p className="nav-text">    ABOUT   </p> </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to='/events'>    <p className="nav-text">    EVENTS  </p> </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to='/subcommittees'> <p className="nav-text"> COMMITTEES </p> </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to='/login'>     <p className="nav-text">    LOGIN   </p> </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
 
                 </div>
+
             </div>
                 );
         
