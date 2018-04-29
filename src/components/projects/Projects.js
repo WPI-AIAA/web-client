605185b6c695dcfb19c87b15fffb0e4ce68060e3
Robaire Galliath <robaire@galliath.com>
2018-04-28 22:13:16 -0400

    Created projects page

---
+++ src/components/projects/Projects.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+import './Projects.css';
+
+class Projects extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+        return (
+            <div className="Projects">
+
+                <p> projects </p>
+
+            </div>
+        );
+
+    }
+}
+
+export default Projects;
